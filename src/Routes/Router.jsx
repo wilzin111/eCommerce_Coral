02602aa30bc896a84d2606368d43042311c7c90a
Willian Massalai Tolfo <willianmassalai@gmail.com>
2023-07-03 15:38:43 -0300

    Add some routes

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from '../Pages/Home/Home'
+import About from '../Pages/About/About'
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path='about' element={<About/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
