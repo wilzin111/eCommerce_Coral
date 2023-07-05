b3adaa0daa67c9c8a5b3af42e0da0444ed012be4
Willian Massalai Tolfo <willianmassalai@gmail.com>
2023-07-05 15:27:25 -0300

    add some global settings

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Footer from '../../Components/Footer/Footer'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <Footer />
+      <div>About</div>
+    </>
   )
 }
 
