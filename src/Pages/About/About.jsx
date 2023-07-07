3d54dec34e773320f37b0e8ae5cef478eacd3bbf
Willian Massalai Tolfo <willianmassalai@gmail.com>
2023-07-07 16:57:29 -0300

    about page ready for testing

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,7 +4,7 @@ import Header from '../../Components/Header/Header'
 import img1 from "../../Assets/Images_about/image1.png"
 import img2 from "../../Assets/Images_about/image2.png"
 import img3 from "../../Assets/Images_about/image3.png"
-import img4 from "../../Assets/Images_ about/image4.png"
+import img4 from "../../Assets/Images_about/image4.png"
 import "./About.css"
 
 const About = () => {
@@ -16,36 +16,41 @@ const About = () => {
           <div className="text">
             <h1 className='about-h-p'>ABOUT</h1>
             <p className='about-h-p'>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. </p>
-            <img className="about-img-top" src={img1} alt="teste" />
+            <img className="about-img-top" src={img1} alt="img1" />
           </div>
         </div>
 
         <div className='about-conteiner-second'>
-          <div>
+          <div className="text">
             <h1 className='about-h-p'>ABOUT</h1>
             <p>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing.  Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. </p>
           </div>
         </div>
 
         <div className='about-conteiner-bag'>
-          <h1 className='about-h-p'>ABOUT</h1>
-          <p className='about-h-p'>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing.  Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. </p>
-          <img src={img2} alt="teste" />
+          <div className='about-conteiner-bag-text'>
+            <h1 className='about-h-p'>ABOUT</h1>
+            <p className='about-h-p'>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing.  Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. </p>
+          </div>
+          <img className='about-imgs-whidth' src={img2} alt="img2" />
         </div>
 
         <div className='about-conteiner-clock'>
-          <h1 className='about-h-p'>ABOUT</h1>
-          <p className='about-h-p'>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing.  Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. </p>
-          <img src={img3} alt="teste" />
+          <div className='about-conteiner-clock-text'>
+            <h1 className='about-h-p'>ABOUT</h1>
+            <p className='about-h-p'>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing.  Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. </p>
+          </div>
+          <img className='about-imgs-whidth' src={img3} alt="img3" />
         </div>
 
         <div className='about-conteiner-gleses'>
-          <h1 className='about-h-p'>ABOUT</h1>
-          <p className='about-h-p'>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing.  Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. </p>
-          <img src={img4} alt="teste" />
+          <div className='about-conteiner-gleses-text'>
+            <h1 className='about-h-p'>ABOUT</h1>
+            <p className='about-h-p'>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing.  Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. </p>
+          </div>
+          <img className='about-imgs-whidth' src={img4} alt="img4" />
         </div>
       </div>
-
 
       <Footer />
     </>
