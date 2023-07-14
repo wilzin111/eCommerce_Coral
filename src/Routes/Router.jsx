0daa9b0c12cc92eeb7506148c4a792afd271aeff
Murilo dos Santos Dessuy <murilodessuy1@gmail.com>
2023-07-14 16:41:24 -0300

    User Profile Page

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home/Home"
 import About from "../Pages/About/About"
+import UserProfile from "../Pages/UserProfile/UserProfile"
 
 const Router = () => {
     return (
@@ -8,6 +9,7 @@ const Router = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path='about' element={<About/>}/>
+                <Route path='userprofile' element={<UserProfile/>}/>
             </Routes>
         </BrowserRouter>
     )
