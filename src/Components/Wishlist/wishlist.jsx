f749cda76be27c8f5e6e81c44bf1ce3d4cb2b44a
Isadora Beckmann <beckmannisadora@gmail.com>
2023-07-22 14:40:11 -0300

    feat: register products - wishlist test erase

--- src/Components/Wishlist/wishlist.jsx
+++ src/Components/Wishlist/wishlist.jsx
@@ -1,47 +1,12 @@
 import "./wishlist.css";
 import Arrow from "../../Assets/Wishlist/arrow.svg";
 import noFavs from "../../Assets/Wishlist/noFavs.png";
-import { useEffect, useState } from "react";
+import img from "../../Assets/Images home/Zara.png";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { db, storage } from "../../FireBaseConnection";
-import { getDownloadURL, ref } from "firebase/storage";
-import { collection, getDocs, query } from "firebase/firestore";
 
 export default function Wishlist() {
   const [isEmpty, setIsEmpty] = useState(false);
-  const [url, setUrl] = useState("");
-  var arr = [];
-
-  const handleUploadImage = async (id) => {
-    const storageRef = storage;
-    const imagemRef = ref(storageRef, `images/products/${id}`);
-
-    const downloadURL = await getDownloadURL(imagemRef);
-
-    setUrl(downloadURL);
-  };
-
-  async function getProducts() {
-    const q = query(collection(db, "products"));
-
-    await getDocs(q).then((value) => {
-      value.forEach((doc) => {
-        let product = {
-          name: doc.data().name,
-          price: doc.data().price,
-          discount: doc.data().discount,
-          id: doc.data().id,
-        };
-
-        handleUploadImage(product.id);
-        arr.push(product);
-      });
-    });
-  }
-
-  useEffect(() => {
-    getProducts();
-  }, [arr]);
 
   return (
     <>
@@ -71,9 +36,10 @@ export default function Wishlist() {
             </div>
           </section>
         ) : (
-          <section className="products-wishlist" id="products-wishlist">
-            <img src={url} />
-          </section>
+          <section
+            className="products-wishlist"
+            id="products-wishlist"
+          ></section>
         )}
       </div>
     </>
