f30d9d8bd585f4ba6f28be87ad69c10393709734
Willian Massalai Tolfo <willianmassalai@gmail.com>
2023-07-22 17:21:38 -0300

    Add login functionality

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,17 +1,48 @@
 import { Link } from 'react-router-dom'
 import './Login.css'
 
+import { auth } from '../../FireBaseConnection'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+
 const login = () => {
+    const navigatL = useNavigate()
+    const [loginEmail, setLoginEmail] = useState('')
+    const [loginPassword, setLoginPassword] = useState('')
+
+    async function handleLogin() {
+        await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+            .then((value) => {
+                console.log("User logged in successfully")
+                navigatL("/home")
+                localStorage.setItem('userLogado', JSON.stringify(value.user.uid))
+            })
+
+            .catch(() => {
+                console.log('User not found')
+            })
+    }
+
     return (
         <div className='login-container'>
             <div className="login-container-input">
-                <input type="text" placeholder="Your Email"/>
-                <input type="password" placeholder="Enter a password"/>
-                <button className='login-btn'>Log in</button>
+                <input type="text"
+                    placeholder="Your Email"
+                    onChange={(e) => setLoginEmail(e.target.value)}
+                />
+
+                <input type="password"
+                    placeholder="Enter a password"
+                    onChange={(e) => setLoginPassword(e.target.value)}
+                />
+
+                <button className='login-btn' onClick={handleLogin}>Log in</button>
+
                 <div className='login-p'>
-                  <p>Don't have an account? <Link to={"/signup"}>Sign up!</Link></p>  
+                    <p>Don't have an account? <Link to={"/signup"}>Sign up!</Link></p>
                 </div>
-                
+
             </div>
 
         </div>
--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -7,7 +7,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth"
 import { useNavigate } from 'react-router-dom'
 
 const Signup = () => {
-  const navigat = useNavigate()
+  const navigatS = useNavigate()
   const inputRef = useRef(null)
   const [userImg, setUserImg] = useState('')
   const [userName, setUserName] = useState('')
@@ -80,7 +80,7 @@ const Signup = () => {
           })
             .then(() => {
               console.log("Registration done successfully")
-              navigat("/")
+              navigatS("/")
             })
             .catch(() => {
               console.log("Unable to register, please try again!")
