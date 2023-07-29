064bede7167721f8eee7f345c9b69f6c3893f672
Willian Massalai Tolfo <willianmassalai@gmail.com>
2023-07-29 00:39:20 -0300

    Add user data collection

---
+++ src/Contexts/dataUser.jsx
@@ -0,0 +1,42 @@
+import { doc, getDoc } from "firebase/firestore";
+import { createContext, useEffect, useState } from "react";
+import { auth, db } from "../FireBaseConnection";
+import { onAuthStateChanged } from "firebase/auth";
+
+export const DataUserContext = createContext({})
+function DataUserProvider({ children }) {
+
+    const [dataUser, setDataUser] = useState('');
+
+    async function getDataUser(e) {
+
+        try {
+            if (!dataUser) {
+                await getDoc(doc(db, 'users',e))
+                    .then((value) => {
+                        setDataUser(value.data());
+                    })
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                getDataUser(user.uid)
+            }
+        })
+    })
+
+    return (
+        <DataUserContext.Provider value={{dataUser}}>
+            {children}
+        </DataUserContext.Provider>
+    )
+}
+
+export default DataUserProvider;
