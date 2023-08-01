12e3485aeab95f399cbf7aeb6b32242d5da5494b
Isadora Beckmann <beckmannisadora@gmail.com>
2023-08-01 17:12:07 -0300

    new arrivals products

--- src/Components/Products/products.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { productContext } from "../../Contexts/productsContext";
-
-const products = () => {
-  const { produtos } = useContext(productContext);
-  return <div>products</div>;
-};
-
-export default products;
