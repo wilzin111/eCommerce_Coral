02e573887cc1625e31834d91cab5b31416d7730f
Isadora Beckmann <beckmannisadora@gmail.com>
2023-08-03 14:08:23 -0300

    new arrivals corrections

--- src/Components/ProductsHome/productsHome.jsx
+++ src/Components/ProductsHome/productsHome.jsx
@@ -3,6 +3,9 @@ import heart from "../../Assets/Icons/wishlist.svg";
 import { Link } from "react-router-dom";
 
 export const ProductsHome = ({ produto }) => {
+  const newPrice = produto.price - produto.price * (produto.discount / 100);
+  const roundedPrice = newPrice.toFixed(2);
+
   return (
     <div className="product-img-home-content">
       <img src={produto.url} alt="Product image" className="product-img-home" />
@@ -10,7 +13,7 @@ export const ProductsHome = ({ produto }) => {
         <div className="products-home-text">
           <span className="products-home-span">{produto.name}</span>
           <span className="products-description-home">{produto.subname}</span>
-          <span className="products-home-span">${produto.price}</span>
+          <span className="products-home-span">${roundedPrice}</span>
         </div>
 
         <Link to={"/wishlist"}>
