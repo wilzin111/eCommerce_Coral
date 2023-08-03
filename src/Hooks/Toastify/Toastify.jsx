091fd3b7ebf5ed9b55afa71ef8d7990b0801edb4
Willian Massalai Tolfo <willianmassalai@gmail.com>
2023-08-03 07:49:55 -0300

    Add config toastify

---
+++ src/Hooks/Toastify/Toastify.jsx
@@ -0,0 +1,43 @@
+import { toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+const success = (sucesso) => {
+    toast.success(`${sucesso}`, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+    });
+}
+
+const error = (erro) => {
+    toast.error(`${erro}`, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+    });
+}
+
+const warn = (aviso) => {
+    toast.warn(`${aviso}`, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+    });
+}
+
+export {success,error,warn}
