73425a4ca57e5905433c65ae719f96eefae0960c
Miguel Castanho Reis <miguelcastanhoreisr@gmail.com>
2023-08-10 13:24:35 -0300

    empty wishlist implementation

--- src/Components/Wishlist/wishlist.jsx
+++ src/Components/Wishlist/wishlist.jsx
@@ -6,12 +6,14 @@ import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "../../FireBaseConnection";
 import "./wishlist.css";
 import { Link } from "react-router-dom";
+import noFavs from "../../Assets/Wishlist/noFavs.png";
 
 export default function Wishlist() {
   const { wishlist, removeFromWishlist } = useWishlist();
   const { dataUser } = useContext(DataUserContext);
 
   const [userWishlist, setUserWishlist] = useState([]);
+  const [isEmpty, setIsEmpty] = useState(false);
 
   useEffect(() => {
     const userWishlistRef = doc(db, "userWishlist", dataUser.uid);
@@ -19,14 +21,16 @@ export default function Wishlist() {
       .then((snapshot) => {
         if (snapshot.exists()) {
           setUserWishlist(snapshot.data().products);
-        } else {
-          setUserWishlist([]);
         }
       })
       .catch((error) => {
         console.error("Error fetching user wishlist from Firestore:", error);
       });
   }, [dataUser.uid]);
+
+  useEffect(() => {
+    setIsEmpty(userWishlist.length === 0);
+  }, [userWishlist]);
 
   const handleRemoveFromWishlist = async (productId, event) => {
     event.preventDefault();
@@ -47,21 +51,40 @@ export default function Wishlist() {
         <h2 className="h2-wishlist">My Wishlist</h2>
       </Link>
 
-      <section className="products-wishlist" id="products-wishlist">
-        {userWishlist.map((product) => (
-          <div key={product.id} className="wishlist-product">
-            <div className="wishlist-product-img">
-              <img src={product.url} alt={product.name} />
-            </div>
-            <div className="wishlist-product-details">
-              <h3 className="wishlist-product-name">{product.name}</h3>
-              <p className="wishlist-product-subname">{product.subname}</p>
-              <span className="wishlist-product-price">${product.price}</span>
-              <button onClick={(event) => handleRemoveFromWishlist(product.id, event)}>Remove</button>
+      {isEmpty ? (
+        <section className="container-wishlist" id="container-wishlist">
+          <div className="img-wishlist">
+            <img src={noFavs} alt="empty wishlist" />
+          </div>
+          <div className="text-wishlist">
+            <h1 className="h1-wishlist">Well...</h1>
+            <div className="p-wishlist">
+              <p>It seems you have not added any products to your wishlist.</p>
             </div>
           </div>
-        ))}
-      </section>
+          <div className="btn-div-wishlist">
+            <Link to={"/"} className="start-shopping">
+              Start Shopping
+            </Link>
+          </div>
+        </section>
+      ) : (
+        <section className="products-wishlist" id="products-wishlist">
+          {userWishlist.map((product) => (
+            <div key={product.id} className="wishlist-product">
+              <div className="wishlist-product-img">
+                <img src={product.url} alt={product.name} />
+              </div>
+              <div className="wishlist-product-details">
+                <h3 className="wishlist-product-name">{product.name}</h3>
+                <p className="wishlist-product-subname">{product.subname}</p>
+                <span className="wishlist-product-price">${product.price}</span>
+                <button type="button" onClick={(event) => handleRemoveFromWishlist(product.id, event)}>Remove</button>
+              </div>
+            </div>
+          ))}
+        </section>
+      )}
     </div>
   );
 }
