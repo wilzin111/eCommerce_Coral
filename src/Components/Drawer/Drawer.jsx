0fcc3b807238cd6f304a180f16cb6a090e577009
Willian Massalai Tolfo <willianmassalai@gmail.com>
2023-08-18 19:35:26 -0300

    fix: animation drawer

--- src/Components/Drawer/Drawer.jsx
+++ src/Components/Drawer/Drawer.jsx
@@ -1,28 +1,14 @@
-import { useContext, useState } from "react";
-import "./Drawer.css";
-import { DataUserContext } from "../../Contexts/dataUser";
-import { storage } from "../../FireBaseConnection";
-import { getDownloadURL, ref } from "firebase/storage";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from 'react'
+import './Drawer.css'
+import { DataUserContext } from '../../Contexts/dataUser'
+import { storage } from '../../FireBaseConnection'
+import { getDownloadURL, ref } from 'firebase/storage'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
 
 const Drawer = ({ isOpen = false, setDrawerOpen }) => {
-  const arrow = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-    >
-      <path
-        d="M9 4.5L16.5 12L9 19.5"
-        stroke="#13101E"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
+  const arrow = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path d="M9 4.5L16.5 12L9 19.5" stroke="#13101E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
 
   const { dataUser, isLog } = useContext(DataUserContext)
   const [urlImgUser, setUrlImgUser] = useState('');
@@ -39,11 +25,21 @@ const Drawer = ({ isOpen = false, setDrawerOpen }) => {
     }
   }
 
+  const [animation, setAnimation] = useState(true)
+
   if (isOpen) {
     return (
-      <div className='drawer-header'>
+      <div className='drawer-header' onClick={(e) => {
+        if (e.target.className === 'drawer-header') {
+          setAnimation(false);
+          setTimeout(() => {
+            setDrawerOpen(false)
+            setAnimation(true);
+          }, 300);
+        }
+      }}>
 
-        <div className='drawer-container-user'>
+        <div className={animation ? 'drawer-container-user animation-header-modal' : 'drawer-container-user animation-close-header-modal'}>
           <div className='user-drawer'>
             <div className='drawer-img-user'>
               <img src={urlImgUser ? urlImgUser : './imgUserNone.jpg'} alt="" />
@@ -54,61 +50,78 @@ const Drawer = ({ isOpen = false, setDrawerOpen }) => {
 
           <hr />
 
-          <div className="drawer-top-categoris">
+          <div className='drawer-top-categoris'>
             <p>Top categories</p>
-            <Link to={"products/Skincare"} className="text-drawer">
-              <p>Skincare</p>
-              <button className="drawer-btn-arrow">{arrow}</button>
+            <Link to={"products/Skincare"}>
+              <div className="text-drawer">
+                <p>Skincare</p>
+                <button className='drawer-btn-arrow'>{arrow}</button>
+              </div>
             </Link>
 
-            <Link to={"products/Apparels"} className="text-drawer">
-              <p>Apparels</p>
-              <button className="drawer-btn-arrow">{arrow}</button>
+            <Link to={'products/Apparels'}>
+              <div className="text-drawer">
+                <p>Apparels</p>
+                <button className='drawer-btn-arrow'>{arrow}</button>
+              </div>
             </Link>
 
-            <Link to={"/products/Jewellery"} className="text-drawer">
-              <p>Jewellery</p>
-              <button className="drawer-btn-arrow">{arrow}</button>
+            <Link to={"/products/Jewellery"}>
+              <div className="text-drawer">
+                <p>Jwellery</p>
+                <button className='drawer-btn-arrow'>{arrow}</button>
+              </div>
             </Link>
 
-            <Link to={"/products/Handbags"} className="text-drawer">
-              <p>Handbags</p>
-              <button className="drawer-btn-arrow">{arrow}</button>
+            <Link to={"/products/Handbags"}>
+              <div className="text-drawer">
+                <p>Handbags</p>
+                <button className='drawer-btn-arrow'>{arrow}</button>
+              </div>
             </Link>
 
-            <Link to={"/products/Eyewear"} className="text-drawer">
-              <p>Eyewear</p>
-              <button className="drawer-btn-arrow">{arrow}</button>
+            <Link to={'/products/Eyewear'}>
+              <div className="text-drawer">
+                <p>EyeWare</p>
+                <button className='drawer-btn-arrow'>{arrow}</button>
+              </div>
             </Link>
 
-            <Link to={"/products/Personal Care"} className="text-drawer">
-              <p>Fragrance</p>
-              <button className="drawer-btn-arrow">{arrow}</button>
+            <Link to={'/products/Personal Care'}>
+              <div className="text-drawer">
+                <p>Fragrance</p>
+                <button className='drawer-btn-arrow'>{arrow}</button>
+              </div>
             </Link>
 
-            <Link to={"/products/Watches"} className="text-drawer">
-              <p>Watches</p>
-              <button className="drawer-btn-arrow">{arrow}</button>
+            <Link to={"/products/Watches"}>
+              <div className="text-drawer">
+                <p>Watches</p>
+                <button className='drawer-btn-arrow'>{arrow}</button>
+              </div>
             </Link>
 
-            <div className="text-drawer">
-              <p>About</p>
-              <button className='drawer-btn-arrow'><Link to={'/about'}>{arrow}</Link></button>
-            </div>
-
+            <Link to={'/about'}>
+              <div className="text-drawer">
+                <p>About</p>
+                <button className='drawer-btn-arrow'>{arrow}</button>
+              </div>
+            </Link>
           </div>
+          
           <hr />
+
           <div className='drawer-ContactUs'>
             <p>Contact Us</p>
 
             <div className="text2-drawer">
               <p>Help & Support </p>
-              <button className="drawer-btn-arrow">{arrow}</button>
+              <button className='drawer-btn-arrow'>{arrow}</button>
             </div>
 
             <div className="text2-drawer">
               <p>Feedback & Suggestions </p>
-              <button className="drawer-btn-arrow">{arrow}</button>
+              <button className='drawer-btn-arrow'>{arrow}</button>
             </div>
 
             <div className="text2-drawer">
@@ -117,12 +130,9 @@ const Drawer = ({ isOpen = false, setDrawerOpen }) => {
             </div>
           </div>
         </div>
-        <button onClick={() => { setDrawerOpen(false) }} className='drawer-btn' />
       </div>
-    );
+    )
   }
-};
+}
 
-export default Drawer;
-
-
+export default Drawer
